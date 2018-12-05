8f121d19454205365122a37bf537a35235b53410
diogo-palma <diogopalma@live.com>
2018-12-05 12:08:27 -0400

    Update animais.sql

--- animais.sql
+++ animais.sql
@@ -1,9 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 CREATE TABLE `animal` (
   `ani_codigo` int(11) NOT NULL,
   `ani_nome` varchar(150) NOT NULL
